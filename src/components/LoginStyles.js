b31bfa15d94833a1b137e94bfaa4d602b9fe4455
salman18 <salmanaleemkhan786@gmail.com>
2019-10-20 15:48:00 +0530

    LoginScreen Design Done

---
+++ src/components/LoginStyles.js
@@ -0,0 +1,44 @@
+import {StyleSheet} from 'react-native';
+import {Platform} from 'react-native';
+
+// import colors from '../../utils/Colors';
+const LoginStyles = StyleSheet.create({
+  OuterContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  Header: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  InputPadding: {
+    padding: 15,
+  },
+  TextInputStyle: {
+    height: 50,
+    marginBottom: 15,
+    fontSize: 20,
+  },
+  ButtonContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  flexCenter: {
+    flex: 1,
+    alignItems: 'center',
+    width: '100%',
+  },
+  LoginButton: {
+    borderRadius: 20,
+    backgroundColor: 'blue',
+    paddingVertical: Platform.OS === 'android' ? 23 : 0,
+    marginBottom: 10,
+    height: 10,
+  },
+  LoginText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
+export default LoginStyles;
---
+++ src/utils/Colors.js
@@ -0,0 +1,15 @@
+const colors = {
+  // border_gray: '#d4d4d4',
+  // black_with_opacity: '#2b2b2b',
+  // grey_with_opacity: 'rgba(43, 43, 43, 0.5)',
+  // black_color: '#000000',
+  // background_grey: '#ebebeb',
+  // bottom_border_orange: '#f46223',
+  button_blue: '#1DA1F2',
+  text_white: 'white',
+  text_gray: '#A9A9A9',
+  color_green: 'green',
+  color_red: 'red',
+};
+
+export default colors;
