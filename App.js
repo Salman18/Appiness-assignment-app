8d4b5e825d417bc14ac849663fbca80cd180c09f
salman18 <salmanaleemkhan786@gmail.com>
2019-10-20 15:52:56 +0530

    LoginScreen Design Done

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Login from './src/components/Login';
-// import NewsListContainer from './src/containers/NewsListContainer';
-// import NewsDetail from './src/components/NewsDetail/NewsDetail';
 
 const Router = createStackNavigator({
   Login: {
